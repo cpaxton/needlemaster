5c0a924487819cbd32c963f11ac50603bb48cd94
Chris Paxton <paxton.chris@live.com>
2014-12-08 17:18:28 -0500

    minor changes to definitions

--- src/grid/predicator/PredicateDefinition.java
+++ src/grid/predicator/PredicateDefinition.java
@@ -1,13 +1,18 @@
 package grid.predicator;
 
+import java.util.ArrayList;
+
 /**
  * Represents a whole class of possible predicates.
  * 
  * @author Chris
  */
 public class PredicateDefinition {
-	String name;
-	int numArgs;
+	String name; // name of this predicate; "ABOVE", "BELOW", "NEXT-TO", etc.
+	int numArgs; // max number of arguments; should match types.size() if set
+	
+	// stores acceptable argument types
+	ArrayList<String> types;
 	
 	String getString(Predicate instance) {
 		if (instance.ofPredicateType(name)) {
